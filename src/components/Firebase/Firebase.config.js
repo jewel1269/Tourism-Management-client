1f56bb065a9ac89b86729e4fead16d24e8b39147
Jewel Mia <145703213+jewel1269@users.noreply.github.com>
2024-04-28 20:41:13 +0600

    environment setup complete

--- src/components/Firebase/Firebase.config.js
+++ src/components/Firebase/Firebase.config.js
@@ -4,13 +4,16 @@ import { initializeApp } from "firebase/app";
 // https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
+
+
+console.log("inside firebase",import.meta.env.VITE_PASS)
 const firebaseConfig = {
-  apiKey: "AIzaSyB1-KgqV-q9yPF6_eQKYu3DZjYRsTfvPWE",
-  authDomain: "tourism-management-website.firebaseapp.com",
-  projectId: "tourism-management-website",
-  storageBucket: "tourism-management-website.appspot.com",
-  messagingSenderId: "1060999094111",
-  appId: "1:1060999094111:web:eef3525c614e2f39cb60ea"
+  apiKey:import.meta.env.VITE_APIKEY,
+  authDomain:import.meta.env.VITE_AUTHDOMAIN,
+  projectId:import.meta.env.VITE_PROJECTID,
+  storageBucket:import.meta.env.VITE_STORAGEBUCKET,
+  messagingSenderId:import.meta.env.VITE_MESSAGINGSENDERID,
+  appId:import.meta.env.VITE_APPID,
 };
 
 // Initialize Firebase
